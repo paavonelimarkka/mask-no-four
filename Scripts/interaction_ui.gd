b84b802db079dcd494e4466c39779a39599ac5e9
loysaranne <juho.talus@gmail.com>
2026-01-31 20:34:46 +0200

    get actual npc mask and face

--- Scripts/interaction_ui.gd
+++ Scripts/interaction_ui.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@export var hair: TextureRect
+@export var eyes: TextureRect
+@export var beard: TextureRect
+@export var mask: TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +15,11 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_show_ui(npc: NPC):
+	hair.texture = npc.hair
+	eyes.texture = npc.eyes
+	beard.texture = npc.beard
+	mask.texture = npc.big_mask
+	mask.modulate = npc.custom_modulate
 	visible = true
 
 func _on_close_ui_pressed() -> void:
