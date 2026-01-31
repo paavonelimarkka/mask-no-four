1e431163f729505c9a0b662a180bdb0a0435418e
Paavo <paavo.nelimarkka@gmail.com>
2026-01-31 19:25:57 +0200

    fix player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,7 +8,7 @@ var nearby_npcs: Array = []
 var can_move: bool = false
 
 func _ready() -> void:
-	can_move == true
+	can_move = true
 	$InteractArea.body_entered.connect(_on_interact_area_body_entered)
 	$InteractArea.body_exited.connect(_on_interact_area_body_exited)
 
@@ -29,7 +29,9 @@ func _physics_process(delta):
 		move_and_slide()
 
 func _process(delta):
+	print(can_move)
 	if Input.is_action_just_pressed("Interact") and nearby_npcs.size() > 0:
 		var npc = nearby_npcs[0]
+		npc.stop_movement()
 		emit_signal("show_ui", npc)
-		can_move == false
+		can_move = false
