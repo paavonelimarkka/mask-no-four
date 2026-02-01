8e84c208d626d63e53341a5470ad6c71eb86c4c8
loysaranne <juho.talus@gmail.com>
2026-02-01 10:55:45 +0200

    add mumbling

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var SFXStreams: Array[AudioStreamOggVorbis]
+@export var SFXPlayers: Array[AudioStreamPlayer2D]
+@export var mumbling_timer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +15,17 @@ func _process(delta: float) -> void:
 func _on_timer_timeout() -> void:
 	print("aikaloppu")
 	get_tree().change_scene_to_file("res://Scenes/start.tscn")
+
+
+func _on_mumbling_timer_timeout() -> void:
+	print("Maybe play")
+	if randi_range(0, 3) >= 1:
+		print("Lets play")
+		mumbling_timer.stop()
+		var sfx_player = SFXPlayers[randi_range(0, SFXPlayers.size() - 1)]
+		sfx_player.stream = SFXStreams[randi_range(0, SFXStreams.size() - 1)]
+		sfx_player.play()
+
+
+func _on_sfx_finished() -> void:
+	mumbling_timer.start()
